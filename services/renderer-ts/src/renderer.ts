ca819739bdd8e67f3fcb73e5737570b351228800
Nep <spica@starry.blue>
2020-08-25 12:03:54 +0900

    implement markdown markup by unified

--- services/renderer-ts/src/renderer.ts
+++ services/renderer-ts/src/renderer.ts
@@ -1,8 +1,27 @@
+import unified from "unified"
+import markdown from "remark-parse"
+import remark2rehype from "remark-rehype"
+import html from "rehype-stringify"
+
 /**
  * 受け取った文書を HTML に変換する
  */
 export async function render(src: string): Promise<string> {
-  // TODO: これはサンプル実装 (URL の自動リンク) です
-  const html = src.replace(/https?:\/\/[^\s]+/g, (url) => `<a href="${encodeURI(url)}" target="_blank">${url}</a>`);
-  return html;
+  const result = await processMarkdown(src);
+
+  return result;
+}
+
+/**
+ * Markdown 記法を HTML に変換する
+ */
+async function processMarkdown(src: string): Promise<string> {
+  const processor = unified()
+    .use(markdown)
+    .use(remark2rehype)
+    .use(html);
+  
+  return processor.process(src).then((res) => {
+    return String(res);
+  });
 }
