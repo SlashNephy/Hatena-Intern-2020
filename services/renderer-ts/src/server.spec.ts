5df7741a9c9a2320243936968458edbc620f4051
Nep <spica@starry.blue>
2020-08-25 15:35:42 +0900

    fix server test

--- services/renderer-ts/src/server.spec.ts
+++ services/renderer-ts/src/server.spec.ts
@@ -4,9 +4,9 @@ import { RenderRequest } from "../pb/renderer/renderer_pb";
 describe("handleRender", () => {
   it("記法変換リクエストを受けて変換できる", async () => {
     const req = new RenderRequest();
-    req.setSrc("foo https://google.com/ bar");
+    req.setSrc("# test");
     const ctx = new Map<string, unknown>();
     const reply = await handleRender(req, ctx);
-    expect(reply.getHtml()).toBe('foo <a href="https://google.com/" target="_blank">https://google.com/</a> bar');
+    expect(reply.getHtml()).toBe('<h1>test</h1>');
   });
 });
