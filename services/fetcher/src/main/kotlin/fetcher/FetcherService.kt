a96c10e9d4857eaba4ba92cecda8a5e0c358b52a
Nep <spica@starry.blue>
2020-08-27 11:27:08 +0900

    add error handling if code != OK

--- services/fetcher/src/main/kotlin/fetcher/FetcherService.kt
+++ services/fetcher/src/main/kotlin/fetcher/FetcherService.kt
@@ -6,8 +6,27 @@ import fetcher.pb.PageTitleResponse
 
 object FetcherService: FetcherServiceGrpcKt.FetcherServiceCoroutineImplBase() {
     override suspend fun getPageTitle(request: PageTitleRequest): PageTitleResponse {
+        val result = runCatching {
+            Http.getPageTitle(request.url)
+        }.onSuccess {
+            logger.info { "gRPC response success: ${request.url} => $it" }
+        }.onFailure {
+            logger.error(it) { "gRPC response failure: ${request.url}" }
+        }
+
         return PageTitleResponse.newBuilder()
-            .setTitle(Http.getPageTitle(request.url))
+            .apply {
+                if (result.isSuccess) {
+                    if (result.getOrNull() != null) {
+                        code = PageTitleResponse.StatusCode.OK
+                        title = result.getOrNull()
+                    } else {
+                        code = PageTitleResponse.StatusCode.UNDEFINED_TITLE
+                    }
+                } else {
+                    code = PageTitleResponse.StatusCode.UNAVAILABLE
+                }
+            }
             .build()
     }
 }
--- services/renderer-ts/src/client.ts
+++ services/renderer-ts/src/client.ts
@@ -1,9 +1,10 @@
-import {PageTitleRequest} from "../pb/fetcher/fetcher_pb"
+import {PageTitleRequest, PageTitleResponse} from "../pb/fetcher/fetcher_pb"
 import {FetcherServiceClient} from "../pb/fetcher/fetcher_grpc_pb";
 import {credentials} from "grpc";
 import {loadConfig} from "./config";
+import StatusCode = PageTitleResponse.StatusCode;
 
-export function getPageTitle(url: string): Promise<string> {
+export function getPageTitle(url: string): Promise<string | undefined> {
     const config = loadConfig();
     const client = new FetcherServiceClient(config.fetcherAddr, credentials.createInsecure());
 
@@ -12,8 +13,22 @@ export function getPageTitle(url: string): Promise<string> {
 
         client.getPageTitle(request, ((error, response) => {
             if (error === null) {
-                const title = response.getTitle();
-                resolve(title);
+                const code = response.getCode();
+                switch (code) {
+                    case PageTitleResponse.StatusCode.OK:
+                        const title = response.getTitle();
+                        resolve(title);
+
+                        break;
+                    case PageTitleResponse.StatusCode.UNDEFINED_TITLE:
+                        resolve(undefined);
+
+                        break;
+                    default:
+                        reject();
+
+                        break;
+                }
             } else {
                 reject(error);
             }
